724daa12df3572395d2d8ec987cf12ea99433ff4
Kenny Chang <ckenny0215@gmail.com>
2020-11-04 17:10:48 +0800

    Update Form.vue

--- components/Form.vue
+++ components/Form.vue
@@ -130,7 +130,7 @@
     },
     methods: {
       sendForm: function() {
-        this.$mail.send({
+        this.$axios.$post('/', {
           from: this.contact_name + '<' + this.contact_email + '>',
           subject: this.company_name + '聯絡需求',
           text: '需求產品：' + this.checked_product + '，需求詳細說明：' + this.detail_content,
