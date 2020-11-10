f0b2db82f5c43f985d07b10e84b5e8f8a79691f8
Kenny Chang <ckenny0215@gmail.com>
2020-11-10 22:23:31 +0800

    email form

--- components/Form.vue
+++ components/Form.vue
@@ -132,9 +132,9 @@
       sendForm: function() {
         this.$axios.$post('/mail/send', {
           from: this.contact_name + '<' + this.contact_email + '>',
-          subject: this.company_name + '聯絡需求',
-          text: '需求產品：' + this.checked_product + '，需求詳細說明：' + this.detail_content,
-          to: ['octopusidea@gmail.com', 'Contact@i-buzz.com.tw']
+          subject: '【i-Buzz官方網站 需求單】 ' + this.company_name + ' ' + this.contact_title + ' ' + this.contact_name,
+          text: '來自' + this.company_name + ' ' + this.contact_title + ' ' + this.contact_name + '的需求' + '\n \n 需求產品：\n ' + this.checked_product + '\n \n 需求詳細說明：\n ' + this.detail_content + '\n \n 公司電話：\n ' + this.company_number + '分機' + this.company_ext + '\n \n 手機：\n ' + this.contact_cell + this.company_ext + '\n \n 電子信箱：\n ' + this.contact_email,
+          to: 'Contact@i-buzz.com.tw'
 
         })
         this.$store.commit('toggleForm')
