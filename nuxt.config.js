4a74665838e5e8f7fe49b8cc0adb75ff8533ff61
Kenny Chang <ckenny0215@gmail.com>
2021-01-25 22:26:23 +0800

    feat: add ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,6 +108,9 @@ export default {
     },
     {
       src: './plugins/vee-validate.js'
+    },
+    { 
+      src: '~plugins/ga.js', mode: 'client' 
     }
   ],
   loading: '~/components/loading.vue',
